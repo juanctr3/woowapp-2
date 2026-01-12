2b10a1ed4ba9e5df236b6bb2999feeab125d3678
Juan Carlos <71723322+juanctr3@users.noreply.github.com>
2026-01-12 13:31:32 -0500

    Update woowapp.php

--- woowapp.php
+++ woowapp.php
@@ -913,9 +913,6 @@ $sql_interactions = "CREATE TABLE IF NOT EXISTS $interactions_table (
     }
 
     /**
-     * ENVIAR MENSAJE - VERSIÓN COMPLETAMENTE REESCRITA v2.2.2
-     */
-    /**
      * ENVIAR MENSAJE - VERSIÓN CON ROMPEHIELOS
      * @param object $cart_row Datos del carrito
      * @param int $message_number Número de mensaje (1, 2, 3)
@@ -2894,6 +2891,7 @@ function wse_pro_show_license_notice_in_settings() {
 }
 // Enganchar antes de que se muestren los campos de ajustes de WooWApp
 add_action('woocommerce_settings_tabs_woowapp', 'wse_pro_show_license_notice_in_settings', 5); // Prioridad 5 para mostrarlo arriba
+
 
 
 
